8600d218b3ff1d20af8b505d2fe8add7677f7fae
Shay <shteymanshay14@gmail.com>
2023-11-09 22:10:01 +0200

    Added acceability to the gallery still not to the firebase storage

--- app/src/main/java/com/example/camtranslator/MainActivity.java
+++ app/src/main/java/com/example/camtranslator/MainActivity.java
@@ -1,23 +1,55 @@
 package com.example.camtranslator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    //components vars
+    private ImageView iv;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.imageView_Gallery);
+
     }
 
     public void logout(View view)
     {
         FirebaseAuth.getInstance().signOut();
         finish();
+    }
+
+    public void gallery(View view)
+    {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK)
+        {
+            if(requestCode == 1000)
+            {
+                iv.setImageURI(data.getData());
+            }
+        }
     }
 }
